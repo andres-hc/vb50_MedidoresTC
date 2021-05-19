00e0c22c3bd2846a9365185a2ae485d4b878c2ad
Andres Hidalgo <andres.ahc@gmail.com>
2021-05-18 23:12:28 -0400

    avanzando

---
+++ MedidorTCApp/MedidorTCModel/DAL/IMedidorDAL.cs
@@ -0,0 +1,16 @@
+﻿using MedidorTCModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorTCModel.DAL
+{
+    public interface IMedidorDAL
+    {
+
+        void Save(Medidor m);
+        List<Medidor> GetAll();
+    }
+}
---
+++ MedidorTCApp/MedidorTCModel/DTO/Medidor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorTCModel.DTO
+{
+    public class Medidor
+    {
+        private Int32 idMedidor;
+        private DateTime fechaConsumo;
+        private string tipo;
+
+        public int IdMedidor { get => idMedidor; set => idMedidor = value; }
+        public DateTime FechaConsumo { get => fechaConsumo; set => fechaConsumo = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+        public override string ToString()
+        {
+            return FechaConsumo + "|" + IdMedidor + "|" + Tipo;
+        }
+    }
+}
